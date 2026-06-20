19adbeb58bdfea5bc15d2b0cdfefbacaafc6ff03
Rintu-chowdory <chowdoryrintu492@gmail.com>
2026-06-20 14:24:25 +0200

    init: tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  corePlugins: { preflight: false },
+  theme: {
+    extend: {
+      colors: {
+        aachen: {
+          blue: '#003D73',
+          gold: '#C8A951',
+          light: '#E8F0F9',
+        }
+      }
+    }
+  },
+  plugins: [],
+}
